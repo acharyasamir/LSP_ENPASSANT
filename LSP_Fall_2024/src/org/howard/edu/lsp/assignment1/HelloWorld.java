10fa9fc88bc71d047b81b5f3f7c1e86e2c3fd5d5
Sameer Acharya <acharyasameer2002@gmail.com>
2024-08-30 12:30:57 -0400

    add assignment1 work

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+*  Name: Samir Acharya
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello LSP!! Excited to learn Java!!!");
+
+	}
+}
